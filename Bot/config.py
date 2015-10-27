c6766ca9e7fe1292f4ad8eec2a812b13a815c3bb
DHikade <info@dhika.de>
2015-10-27 13:30:36 +0100

    Initial commit

---
+++ Bot/config.py
@@ -0,0 +1,37 @@
+'''
+    This is Bot_Omb, a Twitch-Moderation-Bot written in Python and here
+    to help you in any kind of your daily streaming life.
+    
+    For further information you can visit:
+        - http://www.twitch.tv/bot_omb
+        - http://dhika.de/index.php?id=bot_omb
+        - https://github.com/DHikade/Bot_Omb
+        
+    Copyright (C) 2015 Dominik Hikade
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses.
+    
+'''
+
+HOST = "irc.twitch.tv"
+HOST_WHISPER_120 = "199.9.253.120"
+HOST_WHISPER_119 = "199.9.253.119"
+PORT = 6667
+NICK = "YOUR-BOT-NICK"
+PASS = "oauth:AUTH-KEY"
+RATE = 1.5
+VERSION = 1.0
+DEVELOPER = "Serdrad0x"
+CODE = "Python 3.4"
+PATH = "ABSOLUTE-PATH-TO-BOT-FOLDER"
